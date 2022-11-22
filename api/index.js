23242d869a971d37bd6d1e9854d7e863d6179467
Danial-Waheed <daniyalwahied@gmail.com>
2022-11-22 16:13:26 +0500

    File Upload

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 // Create express app
 const app = express();
@@ -32,3 +33,18 @@ mongoose
     });
   })
   .catch((err) => console.log(err));
+
+// file upload
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
