44862639671c374f62d8dbce626e182fc2326527
Danial-Waheed <daniyalwahied@gmail.com>
2022-11-22 12:43:51 +0500

    Write Post Component

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,7 +1,40 @@
 import "./write.css";
 
 const Write = () => {
-  return <div>Write</div>;
+  return (
+    <div className="write">
+      <img
+        className="writeImg"
+        src="https://images.pexels.com/photos/305821/pexels-photo-305821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+      />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fas fa-plus"></i>
+          </label>
+          <input id="fileInput" type="file" style={{ display: "none" }} />
+          <input
+            className="writeInput"
+            placeholder="Title"
+            type="text"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            className="writeInput writeText"
+            placeholder="Tell your story..."
+            type="text"
+            autoFocus={true}
+          />
+        </div>
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Write;
