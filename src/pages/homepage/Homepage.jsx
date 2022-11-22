81c5e1dca9d0800ae5f55f71a2a522075ef4f2f2
Danial-Waheed <daniyalwahied@gmail.com>
2022-11-22 12:32:58 +0500

    sidebar + blogPosts + post components

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,11 +1,16 @@
 import "./homepage.css";
 import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Homepage = () => {
   return (
     <>
       <Header />
-      <div className="home"></div>
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
     </>
   );
 };
